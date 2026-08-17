4d67bcbf99ef321685d9fc44a8cad58bdb59ca74
albt03z <adalberto.gonzales@correo.tdea.edu.co>
2026-08-17 17:04:25 -0500

    feat: add configuration for PostgreSQL database and environment variables

--- pedbox-app-backend/src/app.module.ts
+++ pedbox-app-backend/src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    /**
+     * Carga la configuración de variables de entorno desde el archivo 
+     * .env y la hace disponible globalmente en toda la aplicación.
+     */
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    /**
+     * Configura la conexión a la base de datos PostgreSQL utilizando TypeORM.
+     * La configuración se obtiene de las variables de entorno cargadas por ConfigModule.
+     */
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: configService.get<string>('NODE_ENV') !== 'production',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
